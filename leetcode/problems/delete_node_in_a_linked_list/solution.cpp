175694523754b4bf40cb7028c8037ad9a053f73b
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2020-06-03 05:09:02 +0000

    Sync LeetCode submission - Delete Node in a Linked List (cpp)

--- leetcode/problems/delete_node_in_a_linked_list/solution.cpp
+++ leetcode/problems/delete_node_in_a_linked_list/solution.cpp
@@ -9,7 +9,7 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-       if(node==NULL || node->next==NULL)
+       if(node == NULL || node->next == NULL)
            return;
         node->val = node->next->val;
         node->next = node->next->next;
