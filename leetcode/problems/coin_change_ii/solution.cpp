742403f09e6cb8be8192f69bf15f58dcec4fefa6
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2020-06-07 07:16:20 +0000

    Sync LeetCode submission - Coin Change II (cpp)

---
+++ leetcode/problems/coin_change_ii/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1);
+	    dp[0] = 1;
+	    for (int i=1;i<=coins.size();i++) 
+        {
+            for (int j=1;j<=amount;j++) 
+            {
+                if (j == coins[i-1]) 
+                    dp[j] += 1;
+                if (j - coins[i-1] > 0) 
+                    dp[j] += dp[j - coins[i-1]];
+            }
+	}
+        return dp[amount];
+    }
+};
