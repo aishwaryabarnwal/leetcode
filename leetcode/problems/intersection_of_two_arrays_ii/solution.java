ab2709132f378bec477c142c6d627dfeef56b24f
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2022-02-10 17:42:10 +0000

    Sync LeetCode submission - Intersection of Two Arrays II (java)

---
+++ leetcode/problems/intersection_of_two_arrays_ii/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        //HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] map = new int[1001];
+        int[] res = new int[1001];
+        int count = 0;
+        for(int i:nums1)
+            map[i]++;
+        for(int i:nums2)
+        {
+            if(map[i] > 0)
+            {
+                res[count++] = i;
+                map[i]--;
+            }  
+        }
+        return Arrays.copyOfRange(res, 0, count);
+    }
+}
