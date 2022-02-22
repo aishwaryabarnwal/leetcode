44be5aba0aa1061c353e30104334c2163ac18090
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2022-02-22 17:21:54 +0000

    Sync LeetCode submission - Excel Sheet Column Number (java)

---
+++ leetcode/problems/excel_sheet_column_number/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int res = 0;
+        for(char ch : columnTitle.toCharArray())
+        {
+            int val = (ch - 'A') + 1;
+            res = res * 26 + val;
+        }
+        return res;
+    }
+}
