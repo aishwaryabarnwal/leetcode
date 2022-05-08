60149777db585b49f8b7fb9209f05156386c5f38
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2022-05-08 07:48:35 +0000

    Sync LeetCode submission - Find Target Indices After Sorting Array (java)

--- leetcode/problems/find_target_indices_after_sorting_array/solution.java
+++ leetcode/problems/find_target_indices_after_sorting_array/solution.java
@@ -1,46 +1,39 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        List<Integer> res = new ArrayList<Integer>();
-        
+        List<Integer> res = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
-        int l = 0, h = n, mid = 0;
-        while(l<h)
+        int l = 0, h = n-1, mid = 0;
+        while(l<=h)
         {
-            mid = (l+h)/2;
+            mid = l + (h-l)/2;
             if(nums[mid] == target)
             {
                 res.add(mid);
                 break;
             }
-            else if(nums[mid] < target)
-                l = mid + 1;
-            else
+            else if(nums[mid] > target)
                 h = mid - 1;
+            else
+                l = mid + 1;
         }
         int i = mid - 1, j = mid + 1;
-        while (i >= 0)
+        while(i >= 0)
         {
             if(nums[i] == target)
-            {
-                res.add(i);
-                i--;
-            }
+                res.add(i--);
             else 
                 break;
         }
         while(j < n)
         {
             if(nums[j] == target)
-            {
-                res.add(j);
-                j++;
-            }
+                res.add(j++);
             else 
                 break;
         }
         Collections.sort(res);
-        return  res;
+        return res;
     }
     //     int c = 0, l = 0;
     //     for(int n : nums)
@@ -56,4 +49,5 @@ class Solution {
     //     }
     //     return res;
     // }
+    
 }
