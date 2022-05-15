c2b34c5fdc1cff85d2f0e5d90e0f98ba1af6ff06
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2022-05-15 07:54:20 +0000

    Sync LeetCode submission - Fibonacci Number (java)

--- leetcode/problems/fibonacci_number/solution.java
+++ leetcode/problems/fibonacci_number/solution.java
@@ -1,14 +1,25 @@
 class Solution {
     public int fib(int n) {
-        int prev = 1, prev2 = 0;
-        if(n == 0)
-            return 0;
-        for(int i=2;i<=n;i++)
-        {
-            int curr = prev + prev2;
-            prev2 = prev;
-            prev = curr;
-        }
-        return prev;
+        // int prev = 1, prev2 = 0;
+        // if(n == 0)
+        //     return 0;
+        // for(int i=2;i<=n;i++)
+        // {
+        //     int curr = prev + prev2;
+        //     prev2 = prev;
+        //     prev = curr;
+        // }
+        // return prev;
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return fibonacci(dp, n);
+    }
+    private int fibonacci(int[] dp, int n)
+    {
+        if(n<=1)
+            return n;
+        if(dp[n] != -1)
+            return dp[n];
+        return dp[n] = fibonacci(dp, n-1) + fibonacci(dp, n-2);
     }
 }
