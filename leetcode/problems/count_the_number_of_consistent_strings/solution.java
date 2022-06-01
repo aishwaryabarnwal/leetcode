3a537b9f1282bcb28b4a5010be5103dc6bc5ae59
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2022-06-01 07:53:33 +0000

    Sync LeetCode submission - Count the Number of Consistent Strings (java)

---
+++ leetcode/problems/count_the_number_of_consistent_strings/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        
+        int count  = 0;
+        
+        for (String s : words) {
+            boolean isValid = true;
+            for (char ch : s.toCharArray()) {
+                if (!allowed.contains(String.valueOf(ch))) {
+                    isValid = false;
+                }
+            }
+            if (isValid)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
