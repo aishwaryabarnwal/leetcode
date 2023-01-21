bc6602f64d59593e68c194bd865c3f8a3bb5ba9c
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2023-01-21 16:44:38 +0000

    Sync LeetCode submission - Roman to Integer (java)

--- leetcode/problems/roman_to_integer/solution.java
+++ leetcode/problems/roman_to_integer/solution.java
@@ -1,23 +1,42 @@
 class Solution {
     public int romanToInt(String s) {
         int n = s.length();
-        int num = 0, res = 0;
+        int val = 0, res = 0;
         for(int i=n-1;i>=0;i--)
         {
             switch(s.charAt(i))
             {
-                case 'I' : num = 1; break;
-                case 'V' : num = 5; break;
-                case 'X' : num = 10; break;
-                case 'L' : num = 50; break;
-                case 'C' : num = 100; break;
-                case 'D' : num = 500; break;
-                case 'M' : num = 1000; break;
+                case 'I':
+                    val = 1;
+                    break;
+                case 'V':
+                    val = 5;
+                    break;
+                case 'X':
+                    val = 10;
+                    break;
+                case 'L':
+                    val = 50;
+                    break;
+                case 'C':
+                    val = 100;
+                    break;
+                case 'D':
+                    val = 500;
+                    break;
+                case 'M':
+                    val = 1000;
+                    break;
+
             }
-            if(4 * num < res)
-                res -= num;
+            if(val*4 < res)
+            {
+                res -= val;
+            }
             else
-                res += num;
+            {
+                res += val;
+            }
         }
         return res;
     }
