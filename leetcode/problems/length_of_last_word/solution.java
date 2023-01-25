66e2482aba3a15440e05c5dfd6b11ee737f9c514
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2023-01-25 19:50:10 +0000

    Sync LeetCode submission - Length of Last Word (java)

--- leetcode/problems/length_of_last_word/solution.java
+++ leetcode/problems/length_of_last_word/solution.java
@@ -1,16 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
-        int n = s.length();
-        int count = 0;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(s.charAt(i) == ' ')
-                break;
-            else
-                count++;
-        }
-        return count;
+        String [] val = s.split(" ");
+        return val[val.length-1].length();
+
     }
-    //return s.trim().length()-s.trim().lastIndexOf(" ")-1;
 }
