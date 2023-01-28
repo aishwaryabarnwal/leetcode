87ae4b7bb52b13b17051c9251659b3efe239822a
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2023-01-28 12:23:31 +0000

    Sync LeetCode submission - Sqrt(x) (java)

--- leetcode/problems/sqrt(x)/solution.java
+++ leetcode/problems/sqrt(x)/solution.java
@@ -1,10 +1,13 @@
 class Solution {
     public int mySqrt(int x) {
-        long res = x;
-        while(res * res > x)
+        if(x == 0 || x == 1)
+            return x;
+        int i = 1;
+        for(i=1;i<=x/i;i++)
         {
-            res = (res + x/res) / 2;
+            if(x/i == i)
+                return i;
         }
-        return (int)res;
+        return i-1; //returning (i-1) because at some point (i*i) became greater than x and loop stopped!!
     }
 }
