1dd946a1bf4751b53740f28f53f84e118fe76462
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2023-01-29 15:54:56 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted List (java)

--- leetcode/problems/remove_duplicates_from_sorted_list/solution.java
+++ leetcode/problems/remove_duplicates_from_sorted_list/solution.java
@@ -10,9 +10,18 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if(head == null || head.next == null)
-            return head;
-        head.next = deleteDuplicates(head.next);
-        return head.val == head.next.val ? head.next : head;
+        ListNode temp = head;
+        while(temp!=null && temp.next!=null)
+        {
+            if(temp.val == temp.next.val)
+            {
+                temp.next = temp.next.next;
+            }
+            else
+            {
+                temp = temp.next;
+            }
+        }
+        return head;
     }
 }
