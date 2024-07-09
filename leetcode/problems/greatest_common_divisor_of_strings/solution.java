83d8d13e1109d93ae25e2fb7b69b56df5fc6a39b
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2024-07-09 17:40:02 +0000

    Sync LeetCode submission - Greatest Common Divisor of Strings (java)

--- leetcode/problems/greatest_common_divisor_of_strings/solution.java
+++ leetcode/problems/greatest_common_divisor_of_strings/solution.java
@@ -1,18 +1,13 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        
-        //Two strings str1 and str2 will have gcd if and only 
-        //if str1+str2==str2+str1str1+str2 == str2+str1str1+str2==str2+str1
-        if(!(str1+str2).equals(str2+str1))
+        int n1 = str1.length(), n2 = str2.length();
+        if(!(str1 + str2).equals(str2 + str1))
             return "";
-        //length of the answer or the GCD string 
-        // /our answer will be substring of length 222 from any of the strings str1 or str2.
-        int res = gcd(str1.length(), str2.length());
-            return str2.substring(0, res);
-
+        int gcdNo = gcd(n1, n2);
+        return str2.substring(0, gcdNo);
     }
-    public int gcd(int a, int b)
-    {
-        return (b==0) ? a : gcd(b, a % b);
+    public int gcd(int a, int b) {
+
+        return (b==0)? a : gcd(b, a % b);
     }
 }
