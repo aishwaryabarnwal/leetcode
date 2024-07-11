6f099250e6dcea5b796e062dcb2d792ea3a44c51
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2024-07-11 17:33:11 +0000

    Sync LeetCode submission - Reverse Vowels of a String (java)

--- leetcode/problems/reverse_vowels_of_a_string/solution.java
+++ leetcode/problems/reverse_vowels_of_a_string/solution.java
@@ -1,38 +1,21 @@
 class Solution {
     public String reverseVowels(String s) {
-        int n = s.length();
-        char[] arr = s.toCharArray();
-        int i = 0, j = n-1;
-        while(i<j)
+        Stack<Character> vowels = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray())
         {
-            while(i<j)
-            {
-                if(isVowel(arr[i])) break;
-                    i++;
-            }
-            while(j>i)
-            {
-                if(isVowel(arr[j])) break;
-                    j--; 
-            }
-            if(i >= j)
-                return String.valueOf(arr);
-            swap(arr,i,j);
-            i++;
-            j--;
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' ||ch == 'u' || ch == 'A' || 
+                ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+                vowels.push(ch);
         }
-        return String.valueOf(arr);
-    }
-    public void swap(char[] arr, int i, int j){
-        char temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    public boolean isVowel(char c){
-       char[] vowels = new char[]{'a','e','i','o','u','A','E','I','O','U'};
-       for(int i = 0; i<10; i++){
-           if(c == vowels[i]) return true;
-       }
-       return false;
+        for(char ch : s.toCharArray())
+        {
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' ||ch == 'u' || ch == 'A' || 
+                ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+                sb.append(vowels.pop());
+            else
+                sb.append(ch);
+        }
+        return sb.toString();
     }
 }
