7f2cedbfd8370c246bbf9f7a2d43d5c9441c77e2
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2024-11-13 17:42:08 +0000

    Sync LeetCode submission - Greatest Common Divisor of Strings (java)

--- leetcode/problems/greatest_common_divisor_of_strings/solution.java
+++ leetcode/problems/greatest_common_divisor_of_strings/solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        int n1 = str1.length(), n2 = str2.length();
-        if(!(str1 + str2).equals(str2 + str1))
+        int  n1 = str1.length(), n2 = str2.length();
+       if(!(str1 + str2).equals(str2 + str1))
             return "";
         int gcdNo = gcd(n1, n2);
         return str2.substring(0, gcdNo);
     }
-    public int gcd(int a, int b) {
-
-        return (b==0)? a : gcd(b, a % b);
+    public int gcd(int a, int b)
+    {
+        return b==0? a : gcd(b, a % b);
     }
 }
