7ebad3ddbd9fbc1e8ebbceb1dc38e1510b963076
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-08-23 12:54:15 +0000

    Sync LeetCode submission - Palindrome Number (java)

--- leetcode/problems/palindrome_number/solution.java
+++ leetcode/problems/palindrome_number/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int rev = x, rem = 0, div = 0;
-        if(x < 0)
+        int num = x, x1=0;
+        if (x < 0 || (x % 10 == 0 && x != 0)) 
             return false;
-        while(rev != 0)
+        while(x!=0)
         {
-            rem = rev%10;
-            div = div*10 + rem;
-            rev = rev/10;
+            x1 = x1 * 10 + x % 10;
+            x /= 10;
+
         }
-        if(div == x)
-            return true;
-        return false;
+        return x1 == num;
     }
 }
