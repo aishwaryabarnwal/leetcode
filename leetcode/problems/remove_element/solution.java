07333051854f877bcfd0b4a4bd8b5d522572f9be
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-08-24 08:08:01 +0000

    Sync LeetCode submission - Remove Element (java)

--- leetcode/problems/remove_element/solution.java
+++ leetcode/problems/remove_element/solution.java
@@ -4,9 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i] != val)
-            {
                 nums[j++] = nums[i];
-            }
         }
         return j;
     }
