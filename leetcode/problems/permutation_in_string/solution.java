25bf6d283b168c1528ecce4d01eeeb2d56aa1218
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-09-06 13:39:43 +0000

    Sync LeetCode submission - Permutation in String (java)

--- leetcode/problems/permutation_in_string/solution.java
+++ leetcode/problems/permutation_in_string/solution.java
@@ -1,35 +1,52 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        int n1 = s1.length(), n2 = s2.length();
-        if(s2 == null && n2 == 0)
-            return s1.equals(s2);
-        if(n1 > n2)
-            return false;
-        int[] s1Map = new int[26];
-        int[] s2Map = new int[26];
-        for(int i=0;i<n1;i++)
-        {
-            s1Map[s1.charAt(i) - 'a']++;
-            s2Map[s2.charAt(i) - 'a']++;
+   public boolean checkInclusion(String s1, String s2) {
+    if (s1.length() > s2.length()) return false;
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
+
+        // Frequency of s1
+        for (char c : s1.toCharArray()) {
+            count1[c - 'a']++;
         }
-        for(int i=0;i<(n2-n1);i++)
-        {
-            if(match(s1Map, s2Map))
-                return true;
-            
-            s2Map[s2.charAt(i + n1) - 'a']++;
-            s2Map[s2.charAt(i) - 'a']--;
+
+        int matches = 0;
+        for (int k = 0; k < 26; k++) {
+            if (count1[k] == count2[k]) matches++;
         }
-        return match(s1Map, s2Map);
-        
-    }
-    public boolean match(int[] a1, int[] a2)
-    {
-        for(int i=0;i<26;i++)
-        {
-            if(a1[i] != a2[i])
-                return false;
+
+        int i = 0, j = 0;
+        int n = s2.length();
+        int k = s1.length(); // window size
+
+        while (j < n) {
+            // expand: add s2[j]
+            int idxR = s2.charAt(j) - 'a';
+            count2[idxR]++;
+            if (count2[idxR] == count1[idxR]) {
+                matches++;
+            } else if (count2[idxR] == count1[idxR] + 1) {
+                matches--;
+            }
+
+            // ✅ when window size == k
+            if (j - i + 1 == k) {
+                if (matches == 26) return true;
+
+                // shrink from left
+                int idxL = s2.charAt(i) - 'a';
+                count2[idxL]--;
+                if (count2[idxL] == count1[idxL]) {
+                    matches++;
+                } else if (count2[idxL] == count1[idxL] - 1) {
+                    matches--;
+                }
+                i++;
+            }
+
+            j++;
         }
-        return true;
-    }
+
+        return false;
+   }
 }
