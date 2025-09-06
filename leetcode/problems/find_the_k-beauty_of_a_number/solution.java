7a3ac5b5c8051c64de73517228fb39cddf6829da
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-09-06 09:53:39 +0000

    Sync LeetCode submission - Find the K-Beauty of a Number (java)

--- leetcode/problems/find_the_k-beauty_of_a_number/solution.java
+++ leetcode/problems/find_the_k-beauty_of_a_number/solution.java
@@ -1,19 +1,15 @@
 class Solution {
     public int divisorSubstrings(int num, int k) {
-        String s = String.valueOf(num);
-        int count=0;   
-        for(int i=0;i<s.length()-k+1;i++) 
-        {
-            String temp = s.substring(i,i+k);    
-            int value = Integer.valueOf(temp); 
-            if(value == 0)     
-            {
-                continue;
-            }
-            if(num % value == 0)      
-            {
+        int count = 0;
+        int divisor = (int) Math.pow(10, k); // 10^k
+    
+        int temp = num;
+        while (temp >= divisor / 10) {
+            int subNum = temp % divisor; // last k digits or, k-digit number we extracted
+            if (subNum != 0 && num % subNum == 0) { //if subNum is a divisor of num. num % subNum == 0
                 count++;
             }
+            temp /= 10; // slide window
         }
         return count;
     }
