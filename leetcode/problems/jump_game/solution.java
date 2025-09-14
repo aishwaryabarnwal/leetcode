fa0194d2b4a28c6d5022a2ef0c969cd73b50ac4c
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-09-14 11:14:06 +0000

    Sync LeetCode submission - Jump Game (java)

--- leetcode/problems/jump_game/solution.java
+++ leetcode/problems/jump_game/solution.java
@@ -1,27 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        //dp
-        int n = nums.length;
-        Boolean [] dp = new Boolean[n+1];
-        boolean res = jump(nums, 0, n, dp); 
-        return res;     
-    }
+       int n = nums.length;
+       int maxStep = nums[0];
+       for(int i=0;i<n;i++)
+       {
+            if(maxStep < i)
+                return false;
+            maxStep = Math.max(maxStep, i + nums[i]);
 
-    public boolean jump(int[] nums, int pos, int n, Boolean[] dp)
-    {
-        if(pos == n-1)
-            return dp[pos] = true;
-        if(nums[pos] == 0)
-            return dp[pos] = false;
-        if(dp[pos] != null)
-            return dp[pos];
-        for(int i=pos+1;i<=pos+nums[pos];i++)
-        {
-            if(jump(nums, i, n, dp))
-            {
-                return dp[pos] = true;
-            }
-        }
-        return dp[pos] = false;
+       }
+       return true;
     }
 }
