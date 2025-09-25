1519b150c5e71d82a1f608b8060610fdf304ea75
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-09-25 16:54:50 +0000

    Sync LeetCode submission - H-Index (java)

---
+++ leetcode/problems/h-index/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int h = 0, n = citations.length;
+        for(int i=0;i<n;i++)
+        {
+            int papers = n - i;
+            if(citations[i] >= papers)
+            {
+                h = papers;
+                break;
+            }
+        }
+        return h;
+    }
+}
