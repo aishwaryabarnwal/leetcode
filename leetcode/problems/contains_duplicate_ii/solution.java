471c4eb9551b2e24bebe0a97f42866500ffff5f6
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-09-27 16:04:39 +0000

    Sync LeetCode submission - Contains Duplicate II (java)

--- leetcode/problems/contains_duplicate_ii/solution.java
+++ leetcode/problems/contains_duplicate_ii/solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashSet<Integer> map = new HashSet<>();
-        for(int i=0;i<nums.length;i++)
+        int n = nums.length;
+        Set<Integer> h = new HashSet<>();
+        for(int i=0;i<n;i++)
         {
-            if(i>k) 
-                map.remove(nums[i-k-1]);
-            if(!map.add(nums[i]))
-                 return true;
+            if(!h.add(nums[i]))
+                return true;
+            if(h.size() > k)
+                h.remove(nums[i-k]);
         }
         return false;
     }
