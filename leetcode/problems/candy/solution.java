be8c73a17951f0a04c761a55061b0f3968fa838b
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-11-08 10:02:03 +0000

    Sync LeetCode submission - Candy (java)

--- leetcode/problems/candy/solution.java
+++ leetcode/problems/candy/solution.java
@@ -1,30 +1,22 @@
 class Solution {
     public int candy(int[] ratings) {
-        int sum = 0;
         int n = ratings.length;
-        int[] arr = new int[n];
-        Arrays.fill(arr, 1);
-        for(int i=1;i<n;i++)
-        {
-            if(ratings[i] > ratings[i-1])
-            {
-                arr[i] = arr[i-1] + 1;
+        if (n <= 1) 
+            return n;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+         for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
         }
-        for(int i=n-1;i>0;i--)
-        {
-            if(ratings[i-1] > ratings[i])
-            {
-                if(arr[i-1] <= (arr[i]+1))
-                {
-                    arr[i-1] = arr[i]+1;
-                }
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            sum += arr[i];
-        }
-        return sum;
+        int res = 0;
+        for (int c : candies) res += c;
+            return res;
     }
 }
