d9bc17f65e25dd05389597f39fdf666bfe6b91f2
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-11-22 07:23:31 +0000

    Sync LeetCode submission - Is Subsequence (java)

--- leetcode/problems/is_subsequence/solution.java
+++ leetcode/problems/is_subsequence/solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int s1 = s.length();
-        int t1 = t.length();
-        if(s1 > t1)
+        
+        int sLen = s.length();
+        int tLen = t.length();
+        if(sLen > tLen)
             return false;
-        if(s1<1)
+        if(sLen == 0)
             return true;
         int i = 0, j = 0;
-        while(i < s1 && j <t1)
+        while(i < sLen && j < tLen)
         {
             if(s.charAt(i) == t.charAt(j))
                 i++;
             j++;
-            if(i == s1)
+            if(i == sLen)
                 return true;
         }
         return false;
