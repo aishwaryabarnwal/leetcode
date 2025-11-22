b469a5f68fea9035ea7a9fabe711bafdf3aa78c4
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-11-22 06:57:27 +0000

    Sync LeetCode submission - Coin Change II (java)

---
+++ leetcode/problems/coin_change_ii/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        
+        int n = coins.length;
+        Integer[][] dp = new Integer[n+1][amount+1];
+        return solve(amount, coins, n, dp);
+    }
+
+    public int solve(int amount, int[] coins, int n, Integer[][] dp) {
+        
+        if(amount == 0)
+            return 1;
+        if(n == 0)
+            return 0;
+        if(dp[n][amount] != null)
+            return dp[n][amount];
+        if(coins[n-1] <= amount)
+            return dp[n][amount] = solve(amount-coins[n-1], coins, n, dp) + solve(amount, coins, n-1, dp);
+        else
+            return dp[n][amount] = solve(amount, coins, n-1, dp);
+    }
+}
