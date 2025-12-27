cd6cfee613321138180de1942396f624faf3575c
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-12-27 04:14:50 +0000

    Sync LeetCode submission - Two Sum (java)

--- leetcode/problems/two_sum/solution.java
+++ leetcode/problems/two_sum/solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] res = new int[2];
-        Map<Integer, Integer> map = new HashMap<>();
-        int n = nums.length;
-        for(int i=0;i<n;i++)
-        {
+       int[] res = new int[2];
+       Map<Integer, Integer> map = new HashMap<>();
+       for(int i=0;i<nums.length;i++)
+       {
             int diff = target - nums[i];
             if(map.containsKey(diff))
             {
@@ -13,7 +12,7 @@ class Solution {
                 return res;
             }
             map.put(nums[i], i);
-        }
-        return res;
+       }
+       return res;
     }
 }
