187c46702cff2c486232f72e3794a06caadc83a8
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-12-28 16:38:49 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- leetcode/problems/valid_parentheses/solution.java
+++ leetcode/problems/valid_parentheses/solution.java
@@ -1,26 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
         Deque<Character> st = new ArrayDeque<>();
-        for (char ch : s.toCharArray()) 
+        for(char ch : s.toCharArray())
         {
             if(ch == '(')
-            {
                 st.push(')');
-                continue;
-            }
             else if(ch == '{')
-            {
                 st.push('}');
-                continue;
-            }
             else if(ch == '[')
-            {
                 st.push(']');
-                continue;
-            }
             else if(st.isEmpty() || st.pop() != ch)
                 return false;
+
         }
-        return (st.isEmpty());
+        return st.isEmpty();
     }
 }
