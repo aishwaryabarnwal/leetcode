e984bef940f05062e9d592a51f83d0639ca8acd1
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2025-12-28 17:03:42 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- leetcode/problems/climbing_stairs/solution.java
+++ leetcode/problems/climbing_stairs/solution.java
@@ -1,12 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 2;
-        for(int i=2;i<n;i++)
+        if(n <= 1)
+            return 1;
+        int prev1 = 1, prev2 = 1;
+        for(int i=2;i<=n;i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n-1];
+        return prev1;
     }
 }
