7b8be3c6488975dd7e7f83749899fc2779bfacbb
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2026-01-10 16:03:02 +0000

    Sync LeetCode submission - Single Number (java)

--- leetcode/problems/single_number/solution.java
+++ leetcode/problems/single_number/solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
         int res = 0;
-        for(int i=0;i<n;i++)
+        for(int num : nums)
         {
-            res ^= nums[i];
+            res ^= num;
         }
         return res;
+        
     }
 }
