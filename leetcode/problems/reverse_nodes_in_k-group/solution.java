51b1737c0b29dee9c126cd03b847d432b1252dbe
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2026-01-11 10:22:07 +0000

    Sync LeetCode submission - Reverse Nodes in k-Group (java)

--- leetcode/problems/reverse_nodes_in_k-group/solution.java
+++ leetcode/problems/reverse_nodes_in_k-group/solution.java
@@ -10,42 +10,26 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
-        //base case
-        if(head.next == null) return head;
-        if(k == 1) return head;
-        
-        //define pre and cur
-        ListNode dummy = new ListNode();
-        dummy.next = head;
-        ListNode pre = dummy, cur = head;
-        
-        //reverse the list
-        while(cur != null){
-            boolean space = checkHasSpace(k, cur);
-            if(space){
-                pre = reverse(pre, cur, k);
-                cur = pre.next;
-            }else{
-                break;
-            }
+        ListNode curr = head;
+        int count = 0;
+        while(curr != null && count < k)
+        {
+            count++;
+            curr = curr.next;
         }
-        //return the head
-        return dummy.next;
-    }
-    private boolean checkHasSpace(int k, ListNode cur){
-       for(int i = 0; i < k; i++){
-           if(cur == null) return false;
-           cur = cur.next;
-       }
-        return true;
-    }
-    private ListNode reverse(ListNode pre, ListNode cur, int k){
-        for(int i = 0; i < k - 1; i++){
-            ListNode nex = cur.next;
-            cur.next = nex.next;
-            nex.next = pre.next;
-            pre.next = nex;
+        if(count < k)
+            return head;
+        ListNode prev = reverseKGroup(curr, k);
+        curr = head;
+        while(count > 0)
+        {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+            count--;
         }
-        return cur;
+        return prev;   
+        
     }
 }
