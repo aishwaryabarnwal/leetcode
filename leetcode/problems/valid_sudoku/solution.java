dc9769ce3f655615f6b1dcef0491516f5e921999
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2026-01-17 05:08:03 +0000

    Sync LeetCode submission - Valid Sudoku (java)

--- leetcode/problems/valid_sudoku/solution.java
+++ leetcode/problems/valid_sudoku/solution.java
@@ -1,18 +1,41 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-         Set seen = new HashSet();
-        for(int i=0;i<board.length;i++)
+        for(int i=0;i<9;i++)
         {
-            for(int j=0;j<board[0].length;j++)
+            for(int j=0;j<9;j++)
             {
                 if(board[i][j] != '.')
                 {
-                    String b = "(" + board[i][j] + ")";
-                    if(!seen.add(b+i) || !seen.add(j+b) || !seen.add(i/3 + b + j/3))
-                        return false;
+                    if(!isValid(board, i, j, board[i][j]))
+                    return false;
                 }
             }
         }
         return true;
+    }
+
+    public boolean isValid(char[][] board, int row, int col, char num) {
+        for(int r=0;r<9;r++)
+        {
+            if(r != row && board[r][col] == num)
+                return false;
+        }
+
+        for(int c=0;c<9;c++)
+        {
+            if(c != col && board[row][c] == num)
+                return false;
+        }
+        int r = row / 3 * 3;
+        int c = col / 3 * 3;
+        for(int i=r;i<r+3;i++)
+        {
+            for(int j=c;j<c+3;j++)
+            {
+                if((i != row || j!= col) && board[i][j] == num)
+                    return false;
+            }
+        }
+        return true;    
     }
 }
