df6b629b210ee543505e050268d3e34002436fef
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2026-01-24 03:39:07 +0000

    Sync LeetCode submission - Maximum Product Subarray (java)

--- leetcode/problems/maximum_product_subarray/solution.java
+++ leetcode/problems/maximum_product_subarray/solution.java
@@ -1,18 +1,16 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int n = nums.length;
-        int pre = 1, suff = 1;
-        int ans = Integer.MIN_VALUE;
-        for(int i=0;i<n;i++)
+        int maxSoFar = nums[0];
+        int minSoFar = nums[0];
+        int result = nums[0];
+        for(int i=1;i<nums.length;i++)
         {
-            if(pre == 0)    
-                pre =1;
-            if(suff == 0)
-                suff = 1;
-            pre *= nums[i];
-            suff *= nums[n-i-1];
-            ans = Math.max(ans, Math.max(pre, suff));
+            int curr = nums[i];
+            int tempMax = Math.max(curr, Math.max(maxSoFar * curr, minSoFar * curr));
+            minSoFar = Math.min(curr, Math.min(maxSoFar * curr, minSoFar * curr));
+            maxSoFar = tempMax;
+            result = Math.max(result, maxSoFar);
         }
-        return ans;
+        return result;
     }
 }
