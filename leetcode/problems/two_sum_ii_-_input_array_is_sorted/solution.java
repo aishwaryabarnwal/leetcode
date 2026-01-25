a8207c4570025153013f6baa071579bb8322ff36
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2026-01-25 08:38:44 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted (java)

--- leetcode/problems/two_sum_ii_-_input_array_is_sorted/solution.java
+++ leetcode/problems/two_sum_ii_-_input_array_is_sorted/solution.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int l = 0, r = numbers.length-1;
-        while(numbers[l] + numbers[r] != target)
-        {
-            if(numbers[l] + numbers[r] < target)
+       int l = 0, r = numbers.length - 1;
+       while (l < r)
+       {
+            int sum = numbers[l] + numbers[r];
+            if(sum == target)
+                return new int[] {l+1, r+1};
+            if(sum < target)
                 l++;
             else
                 r--;
-        }
-        return new int[] {l+1, r+1};
+       }
+       return new int[] {-1, -1};
     }
 }
