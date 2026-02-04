d767e6267dc6245f8927845c645e05bc5a00163f
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2026-02-04 05:07:34 +0000

    Sync LeetCode submission - Word Ladder (java)

---
+++ leetcode/problems/word_ladder/solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        
+        Set<String> dict = new HashSet<>(wordList);
+        if(!dict.contains(endWord))
+            return 0;
+        
+        Queue<String> q = new LinkedList<>();
+        q.offer(beginWord);
+        int level = 1;
+
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            for(int i=0;i<size;i++)
+            {
+                String word = q.poll();
+                if(word.equals(endWord))
+                    return level;
+
+                char[] arr = word.toCharArray();
+                for(int j=0;j<arr.length;j++)
+                {
+                    char original = arr[j];
+
+                    for(char ch='a';ch<='z';ch++)
+                    {
+                        arr[j] = ch;
+                        String newWord = new String(arr);
+                        if(dict.contains(newWord))
+                        {
+                            q.offer(newWord);
+                            dict.remove(newWord);
+                        }
+                    }
+                    arr[j] = original;
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+}
