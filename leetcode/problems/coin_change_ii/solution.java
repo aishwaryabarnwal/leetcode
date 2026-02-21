c047ab64bf58e24108f30a2a83aa2f3c3f1caddb
Aishwarya Barnwal <56435576+aishwaryabarnwal@users.noreply.github.com>
2026-02-21 17:25:34 +0000

    Sync LeetCode submission - Coin Change II (java)

--- leetcode/problems/coin_change_ii/solution.java
+++ leetcode/problems/coin_change_ii/solution.java
@@ -1,17 +1,21 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[] dp = new int[amount+1]; //dp[j] = number of ways to make amount j
+         int n = coins.length;
+        Integer[][] dp = new Integer[n+1][amount+1];
+        return solve(amount, coins, n, dp);
+    }
 
-        dp[0] = 1;
-        for(int coin : coins)
-        {
-            for(int j=coin;j<=amount;j++)
-            {
-                dp[j] += dp[j - coin]; // For each coin
-            }
-        }
-        return dp[amount];
-
+    public int solve(int amount, int[] coins, int n, Integer[][] dp) {
+        
+        if(amount == 0)
+            return 1;
+        if(n == 0)
+            return 0;
+        if(dp[n][amount] != null)
+            return dp[n][amount];
+        if(coins[n-1] <= amount)
+            return dp[n][amount] = solve(amount-coins[n-1], coins, n, dp) + solve(amount, coins, n-1, dp);
+        else
+            return dp[n][amount] = solve(amount, coins, n-1, dp);
     }
 }
